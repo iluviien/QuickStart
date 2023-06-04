02cb33c39b9c39c9c4711cc497e3e4a5fa8b0691
iluviien <stefanica695@gmail.com>
2023-06-04 12:12:31 +0000

    add

---
+++ typescriptmadness./src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Person } from "./Person";
+
+function App() {
+  return (
+    <div className="App">
+      <Person
+        name="Pedro"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["jake", "jessica", "jerry"]}
+      />
+    </div>
+  );
+}
+
+export default App;
